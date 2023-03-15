dabeaefeffbf37bc95ef1920c9af88166681b4f9
Loic D <loic.danj@gmail.com>
2023-03-15 15:09:42 +0100

    Allows the user to search for a city and save it

--- Sources/WeatherAppComposant/ViewModels/CitiesWeatherViewModel.swift
+++ Sources/WeatherAppComposant/ViewModels/CitiesWeatherViewModel.swift
@@ -8,25 +8,22 @@
 import Foundation
 
 public class CitiesWeatherViewModel: ObservableObject {
-    public let citySearchVM: CitySeachViewModel
+    public let citySearchVM: CitySearchViewModel
     public let weatherDataFetcher: WeatherDataFetcher
     @Published public var cities: [CityWeather]
     
     public init(apiKey: String) {
-        cities = [CityWeather(name: "Test",
-                              latitude: "",
-                              longitude: "",
-                              weather: nil)]
+        cities = SaveManager.getCities()
         
-        citySearchVM = CitySeachViewModel()
+        citySearchVM = CitySearchViewModel()
         weatherDataFetcher = WeatherDataFetcher(apiKey: apiKey)
         
         citySearchVM.citiesWeatherVM = self
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.cities.first?.weather = WeatherData(currentTemp: "21",
-                                                    dailyWeather: [DailyWeather(minTemp: "5", maxTemp: "12", weatherType: .rain),
-                                                                   DailyWeather(minTemp: "6", maxTemp: "14", weatherType: .sun)])
-        }
+    }
+    
+    public func addCity(_ city: CityWeather) {
+        cities.append(city)
+        SaveManager.saveCities(cities)
+        // TODO: weatherDataFetcher.updateWeather
     }
 }
--- Sources/WeatherAppComposant/ViewModels/CitySearchViewModel.swift
+++ Sources/WeatherAppComposant/ViewModels/CitySearchViewModel.swift
@@ -7,8 +7,42 @@
 
 import Foundation
 
-// Handle city search and add the choosen city to CitiesWeatherViewModel
+import Foundation
+import MapKit
+import Combine
 
-public class CitySeachViewModel: ObservableObject {
+public class CitySearchViewModel: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
     public weak var citiesWeatherVM: CitiesWeatherViewModel?
+    @Published public var searchQuery = ""
+    public var completer: MKLocalSearchCompleter
+    @Published public var completions: [MKLocalSearchCompletion] = []
+    var cancellable: AnyCancellable?
+    
+    override init() {
+        completer = MKLocalSearchCompleter()
+        completer.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        super.init()
+        cancellable = $searchQuery.assign(to: \.queryFragment, on: self.completer)
+        completer.delegate = self
+    }
+    
+    public func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.completions = completer.results
+    }
+    
+    public func addPlace(place: MKLocalSearchCompletion) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString("\(place.title), \(place.subtitle)") { [self] placemarks, error in
+            let placemark = placemarks?.first
+            let lat = placemark?.location?.coordinate.latitude ?? 0
+            let lon = placemark?.location?.coordinate.longitude ?? 0
+            print("Lat: \(lat), Lon: \(lon)")
+            
+            citiesWeatherVM?.addCity(CityWeather(name: place.title,
+                                                 latitude: "\(lat)",
+                                                 longitude: "\(lon)"))
+        }
+    }
 }
+
+extension MKLocalSearchCompletion: Identifiable {}

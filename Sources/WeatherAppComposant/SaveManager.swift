71fa0d8bfaee85fa993a21f37b785034d70bd82a
Loic D <loic.danj@gmail.com>
2023-03-15 14:50:16 +0100

    Save and load cities

--- Sources/WeatherAppComposant/SaveManager.swift
+++ Sources/WeatherAppComposant/SaveManager.swift
@@ -10,5 +10,17 @@ import Foundation
 // Save cities and weather
 
 public class SaveManager {
+    public static func saveCities(_ cities: [CityWeather]) {
+        if let encoded = try? JSONEncoder().encode(cities) {
+            UserDefaults.standard.set(encoded, forKey: "Cities")
+        }
+    }
     
+    public static func getCities() -> [CityWeather] {
+        if let data = UserDefaults.standard.object(forKey: "Cities") as? Data,
+            let cities = try? JSONDecoder().decode([CityWeather].self, from: data) {
+            return cities
+        }
+        return []
+    }
 }
